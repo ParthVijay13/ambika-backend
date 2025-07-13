abffe34f314d9cb808e69213e007ed70b5b7e0a8
ParthVijay13 <parthvijay2004@gmail.com>
2025-07-13 16:03:12 +0530

    things left to add in schema

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,40 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+   {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'", 
+            'data:', 
+            'blob:', 
+            'market-assets.strapi.io', 
+            'res.cloudinary.com'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      headers: '*',
+      origin: ['http://localhost:1337', 'http://localhost:3000', 'http://localhost:3001']
+    }
+  },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- src/api/real-estate2/controllers/real-estate2.js
+++ src/api/real-estate2/controllers/real-estate2.js
@@ -6,4 +6,74 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::real-estate2.real-estate2');
+module.exports = createCoreController('api::real-estate2.real-estate2', ({ strapi }) => ({
+  async find(ctx) {
+    strapi.log.info('Custom find controller called');
+    
+    // Set populate for media fields and dynamic zones with nested images
+    ctx.query.populate = {
+      hero_image: true,
+      section: {
+        on: {
+          'title.section': {
+            populate: {
+              image: true
+            }
+          }
+        }
+      },
+      section2_intro: true,
+      section2_media: {
+        on: {
+          'title.section2-media': {
+            populate: {
+              image: true
+            }
+          }
+        }
+      },
+      section2_points: true,
+      section3_intro: true,
+      section3_points: true
+    };
+    
+    // Call the parent method
+    const result = await super.find(ctx);
+    return result;
+  },
+
+  async findOne(ctx) {
+    strapi.log.info(`Custom findOne controller called for id: ${ctx.params.id}`);
+    
+    // Set populate for media fields and dynamic zones with nested images
+    ctx.query.populate = {
+      hero_image: true,
+      section: {
+        on: {
+          'title.section': {
+            populate: {
+              image: true
+            }
+          }
+        }
+      },
+      section2_intro: true,
+      section2_media: {
+        on: {
+          'title.section2-media': {
+            populate: {
+              image: true
+            }
+          }
+        }
+      },
+      section2_points: true,
+      section3_intro: true,
+      section3_points: true
+    };
+    
+    // Call the parent method
+    const result = await super.findOne(ctx);
+    return result;
+  }
+}));
